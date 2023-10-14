936b39c55c8f9af1e72b497fa985bde478c23e7f
Lukas Polak <admin@lukaspolak.cz>
2023-10-15 00:26:02 +0200

    Added a sample tkinter window & video edit

--- editor.py
+++ editor.py
@@ -1,7 +1,21 @@
+#!/usr/bin/env python3
+
 import moviepy.editor as mp
+import tkinter as tk
 
 
 def main():
+    window = tk.Tk()
+    window.title("Video Editor")
+    window.geometry("400x400")
+    window.mainloop()
+
+    print("Starting the program")
+    newvideo = mp.VideoFileClip("myvideo.mp4").subclip(50, 60)
+    print("Video loaded")
+    newvideo.write_videofile("myvideo2.mp4")
+    print("Video saved")
+
     pass
 
 
